dad011643f99c7ee5b8c73fef685c55ebfe1fe69
Quosain Hasan <33932120+quosainhasan@users.noreply.github.com>
2024-06-23 21:13:17 +0530

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -5,7 +5,7 @@ document.querySelector('nav button[type="button"]').addEventListener('click', fu
 
 document.addEventListener("DOMContentLoaded", function () {
     // Set the date to countdown to (replace with your desired date)
-    const targetDate = new Date("2024-11-02T09:00:00");
+    const targetDate = new Date("2024-11-01T09:00:00");
 
     // Update the countdown every second
     setInterval(updateCountdown, 1000);
